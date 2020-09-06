cb280e5deb6938897172cb7de5f599eaf6c41267
bebyx <bebyx@protonmail.com>
2020-09-07 00:23:30 +0300

    Add frontend, move db dumps

--- k8_run.sh
+++ k8_run.sh
@@ -3,7 +3,16 @@ kubectl run db --image=mariadb --env="MYSQL_ALLOW_EMPTY_PASSWORD=true" \
                                --env="MYSQL_USER=dtapi" \
                                --env="MYSQL_PASSWORD=password"
 
-kubectl exec -i db -- mysql -u root dtapi < dtapi_full.sql
-kubectl exec -i db -- mysql -u root dtapi < sessions.sql
+kubectl exec -i db -- mysql -u root dtapi < database/dtapi_full.sql
+kubectl exec -i db -- mysql -u root dtapi < database/sessions.sql
 
 kubectl expose pod db --type=ClusterIP --port=3306
+
+
+#kubectl create deployment be --image=eu.gcr.io/trainingground-285720/be
+#kubectl expose deployment be --type=NodePort --port=80
+kubectl create -f be-deployment.yaml
+kubectl create -f be-service.yaml
+
+#kubectl create deployment fe --image=eu.gcr.io/trainingground-285720/fe
+#kubectl expose deployment fe --type=NodePort --port=80
