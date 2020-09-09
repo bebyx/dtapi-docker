a8094c2e12cb3f476854e03fe242d96477a2e9ef
bebyx <bebyx@protonmail.com>
2020-09-09 13:22:54 +0300

    Edit config maps for GKE

--- k8_run.sh
+++ k8_run.sh
@@ -7,20 +7,20 @@
 #kubectl exec -i db -- mysql -u root dtapi < database/sessions.sql
 
 #kubectl expose pod db --type=ClusterIP --port=3306
-kubectl create -f db-deployment.yaml
-kubectl create -f db-pv.yaml
-kubectl create -f db-service.yaml
+kubectl apply -f db-deployment.yaml
+kubectl apply -f db-pv.yaml
+kubectl apply -f db-service.yaml
 #kubectl exec -i db -- mysql -u root dtapi < database/dtapi_full.sql
 #kubectl exec -i db -- mysql -u root dtapi < database/sessions.sql
 
 #kubectl create deployment be --image=eu.gcr.io/trainingground-285720/be
 #kubectl expose deployment be --type=NodePort --port=80
-kubectl create -f be-deployment.yaml
-kubectl create -f be-service.yaml
+kubectl apply -f be-deployment.yaml
+kubectl apply -f be-service.yaml
 
 #kubectl create deployment fe --image=eu.gcr.io/trainingground-285720/fe
 #kubectl expose deployment fe --type=NodePort --port=80
-kubectl create -f fe-deployment.yaml
-kubectl create -f fe-service.yaml
+kubectl apply -f fe-deployment.yaml
+kubectl apply -f fe-service.yaml
 
-kubectl create -f ingress.yaml
+kubectl apply -f ingress.yaml
